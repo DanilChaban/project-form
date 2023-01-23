75b3121cf6618b1cbeeeac03712673a102f5a16e
Danil Chaban <krigerblood@gmail.com>
2023-01-23 10:42:10 +0200

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', c}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
